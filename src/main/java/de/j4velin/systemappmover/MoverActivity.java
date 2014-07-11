d84379649cd95e14f469b3272e4d47a66073492c
j4velin <j4velin@gmail.com>
2014-07-11 10:44:04 +0200

    added initial warning dialog

--- src/main/java/de/j4velin/systemappmover/MoverActivity.java
+++ src/main/java/de/j4velin/systemappmover/MoverActivity.java
@@ -20,6 +20,7 @@ import com.stericson.RootTools.*;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -27,120 +28,161 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * The main activity.
- * 
+ *
  * All the logic starts in the AppPicker, which is started from the checkForRoot
  * method if root is available
- * 
  */
 public class MoverActivity extends Activity {
 
-	final static String SYSTEM_APP_FOLDER = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT ? "/system/priv-app/"
-			: "/system/app/";
+    final static String SYSTEM_APP_FOLDER = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT ? "/system/priv-app/"
+            : "/system/app/";
 
-	/**
-	 * Shows an error dialog with the specified text
-	 * 
-	 * @param text
-	 *            the error text
-	 */
-	void showErrorDialog(final String text) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Error").setMessage(text).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-			public void onClick(final DialogInterface dialog, int id) {
-				try {
-					dialog.dismiss();
-				} catch (Exception e) {
-				}
-			}
-		});
-		builder.create().show();
-	}
+    /**
+     * Shows an error dialog with the specified text
+     *
+     * @param text the error text
+     */
+    void showErrorDialog(final String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error").setMessage(text).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(final DialogInterface dialog, int id) {
+                try {
+                    dialog.dismiss();
+                } catch (Exception e) {
+                }
+            }
+        });
+        builder.create().show();
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		RootTools.debugMode = false;
-		checkForRoot();
-	}
+    /**
+     * Shows the initial warning dialog
+     */
+    void showWarningDialog() {
+        final Dialog d = new Dialog(this);
+        d.setTitle("Warning");
+        d.setCancelable(false);
+        d.setContentView(R.layout.warningdialog);
 
-	/**
-	 * Uses the RootTools library to check for root and busybox
-	 */
-	private void checkForRoot() {
-		final ProgressDialog progress = ProgressDialog.show(this, "", "Waiting for root access", true);
-		progress.show();
-		final TextView error = (TextView) findViewById(R.id.error);
-		final Handler h = new Handler();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				if (!RootTools.isRootAvailable()) {
-					if (progress == null || !progress.isShowing())
-						return;
-					progress.cancel();
-					h.post(new Runnable() {
-						@Override
-						public void run() {
-							error.setText("Your device seems not to be rooted!\nThis app requires root access and does not work without.\n\nClick [here] to uninstall.");
-							// ask user to delete app on non-rooted devices
-							error.setOnClickListener(new View.OnClickListener() {
-								@Override
-								public void onClick(View v) {
-									startActivity(new Intent(Intent.ACTION_DELETE, Uri.parse("package:de.j4velin.systemappmover")));
-								}
-							});
-						}
-					});
-					return;
-				}
-				final boolean root = RootTools.isAccessGiven();
-				if (progress == null || !progress.isShowing())
-					return;
-				progress.cancel();
-				h.post(new Runnable() {
-					@Override
-					public void run() {
-						if (root) {
-							((CheckBox) findViewById(R.id.root)).setChecked(true);
-						} else {
-							error.setText("No root access granted - click here to recheck");
-							error.setOnClickListener(new View.OnClickListener() {
-								@Override
-								public void onClick(View v) {
-									checkForRoot();
-								}
-							});
-							return;
-						}
+        final CheckBox c = (CheckBox) d.findViewById(R.id.c);
+        final Button b = (Button) d.findViewById(R.id.b);
 
-						if (RootTools.isBusyboxAvailable()) {
-							CheckBox busyBox = (CheckBox) findViewById(R.id.busybox);
-							busyBox.setChecked(true);
-							busyBox.setText("BusyBox " + RootTools.getBusyBoxVersion());
-							if (root)
-								new AppPicker(MoverActivity.this).execute();
-						} else {
-							error.setText("No busybox found!\nClick here to download");
-							error.setOnClickListener(new View.OnClickListener() {
-								@Override
-								public void onClick(View v) {
-									RootTools.offerBusyBox(MoverActivity.this);
-								}
-							});
+        c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                b.setText(checked ? android.R.string.ok : android.R.string.cancel);
+            }
+        });
 
-							return;
-						}
-						error.setText("Use at your own risk! I won't take responsibility for damages on your device!");
-					}
-				});
-			}
-		}).start();
-	}
+        b.setText(android.R.string.cancel);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (c.isChecked()) {
+                    getSharedPreferences("settings", MODE_PRIVATE).edit().putBoolean("warningRead", true).commit();
+                    d.dismiss();
+                } else {
+                    finish();
+                }
+            }
+        });
+
+        d.show();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        RootTools.debugMode = false;
+        checkForRoot();
+    }
+
+    /**
+     * Uses the RootTools library to check for root and busybox
+     */
+    private void checkForRoot() {
+        final ProgressDialog progress = ProgressDialog.show(this, "", "Waiting for root access", true);
+        progress.show();
+        final TextView error = (TextView) findViewById(R.id.error);
+        final Handler h = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (!RootTools.isRootAvailable()) {
+                    if (progress == null || !progress.isShowing())
+                        return;
+                    progress.cancel();
+                    h.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            error.setText("Your device seems not to be rooted!\nThis app requires root access and does not work without.\n\nClick [here] to uninstall.");
+                            // ask user to delete app on non-rooted devices
+                            error.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    startActivity(new Intent(Intent.ACTION_DELETE, Uri.parse("package:de.j4velin.systemappmover")));
+                                }
+                            });
+                        }
+                    });
+                    return;
+                }
+                final boolean root = RootTools.isAccessGiven();
+                if (progress == null || !progress.isShowing())
+                    return;
+                progress.cancel();
+                h.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (root) {
+                            ((CheckBox) findViewById(R.id.root)).setChecked(true);
+                        } else {
+                            error.setText("No root access granted - click here to recheck");
+                            error.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    checkForRoot();
+                                }
+                            });
+                            return;
+                        }
+
+                        if (RootTools.isBusyboxAvailable()) {
+                            CheckBox busyBox = (CheckBox) findViewById(R.id.busybox);
+                            busyBox.setChecked(true);
+                            busyBox.setText("BusyBox " + RootTools.getBusyBoxVersion());
+                            if (root) {
+                                new AppPicker(MoverActivity.this).execute();
+                                if (!getSharedPreferences("settings", MODE_PRIVATE).getBoolean("warningRead", false)) {
+                                    showWarningDialog();
+                                }
+                            }
+                        } else {
+                            error.setText("No busybox found!\nClick here to download");
+                            error.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    RootTools.offerBusyBox(MoverActivity.this);
+                                    finish();
+                                }
+                            });
+
+                            return;
+                        }
+                        error.setText("Use at your own risk! I won't take responsibility for damages on your device!");
+                    }
+                });
+            }
+        }).start();
+    }
 
 }
