b39005128d62cea8ceae8bf894421c63e69d7088
j4velin <j4velin@gmail.com>
2014-10-12 10:15:14 +0200

    using String::compareTo for sorting the app labels

--- src/main/java/de/j4velin/systemappmover/AppPicker.java
+++ src/main/java/de/j4velin/systemappmover/AppPicker.java
@@ -16,12 +16,6 @@
 
 package de.j4velin.systemappmover;
 
-import java.util.ArrayList;
-
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import android.app.ProgressDialog;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -29,84 +23,78 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 
 /**
  * Class to list all installed app.
- * 
+ * <p/>
  * The actual moving happens in the AppClickListener class when an item is clicked.
- * 
  */
 public class AppPicker extends AsyncTask<Void, Void, Void> {
 
-	List<Drawable> icons;
-	List<ApplicationInfo> apps;
-	PackageManager pm;
-	private ProgressDialog progress;
-	final MoverActivity activity;
+    List<Drawable> icons;
+    List<ApplicationInfo> apps;
+    PackageManager pm;
+    private ProgressDialog progress;
+    final MoverActivity activity;
 
-	public AppPicker(final MoverActivity a) {
-		activity = a;
-	}
+    public AppPicker(final MoverActivity a) {
+        activity = a;
+    }
 
-	@Override
-	protected void onPreExecute() {
-		pm = activity.getPackageManager();
-		progress = ProgressDialog.show(activity, "", "Loading apps", true);
-	}
+    @Override
+    protected void onPreExecute() {
+        pm = activity.getPackageManager();
+        progress = ProgressDialog.show(activity, "", "Loading apps", true);
+    }
 
-	@Override
-	protected void onPostExecute(Void a) {
-		try {
-			progress.cancel();
-		} catch (IllegalArgumentException e) {
-			if (BuildConfig.DEBUG)
-				Logger.log(e);
-		}
-		if (apps == null || apps.isEmpty()) {
-			activity.showErrorDialog("Error loadings apps!");
-		} else {
-			ListView liste = (ListView) activity.findViewById(R.id.apps);
-			liste.setAdapter(new EfficientAdapter(activity, this));
-			liste.setOnItemClickListener(new AppClickListener(this));
-		}
-	}
+    @Override
+    protected void onPostExecute(Void a) {
+        try {
+            progress.cancel();
+        } catch (IllegalArgumentException e) {
+            if (BuildConfig.DEBUG) Logger.log(e);
+        }
+        if (apps == null || apps.isEmpty()) {
+            activity.showErrorDialog("Error loadings apps!");
+        } else {
+            ListView liste = (ListView) activity.findViewById(R.id.apps);
+            liste.setAdapter(new EfficientAdapter(activity, this));
+            liste.setOnItemClickListener(new AppClickListener(this));
+        }
+    }
 
-	@Override
-	protected Void doInBackground(Void... params) {
-		// load all apps and their icons, sort them alphabetical
-		apps = pm.getInstalledApplications(0);
-		try {
-			Collections.sort(apps, new Comparator<ApplicationInfo>() {
-				public int compare(final ApplicationInfo app1, final ApplicationInfo app2) {
-					final CharSequence label1 = app1.loadLabel(pm);
-					final CharSequence label2 = app2.loadLabel(pm);
-					if (label1 == null || label1.length() == 0 || label2 == null || label2.length() == 0) {
-						return 0;
-					} else {
-						try {
-							int pos = 0;
-							while (label1.length() > pos + 1 && label2.length() > pos + 1
-									&& label1.charAt(pos) == label2.charAt(pos)) {
-								pos++;
-							}
-							return (label1.charAt(pos) - label2.charAt(pos));
-						} catch (NullPointerException npe) {
-							return 0;
-						}
-					}
-				}
-			});
-		} catch (IllegalArgumentException iae) {
-		}
+    @Override
+    protected Void doInBackground(Void... params) {
+        // load all apps and their icons, sort them alphabetical
+        apps = pm.getInstalledApplications(0);
+        try {
+            Collections.sort(apps, new Comparator<ApplicationInfo>() {
+                public int compare(final ApplicationInfo app1, final ApplicationInfo app2) {
+                    try {
+                        return app1.loadLabel(pm).toString()
+                                .compareTo(app2.loadLabel(pm).toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return 0;
+                    }
+                }
+            });
+        } catch (IllegalArgumentException iae) {
+        }
 
-		icons = new ArrayList<Drawable>(apps.size());
-		try {
-			for (int i = 0; i < apps.size(); i++) {
-				icons.add(apps.get(i).loadIcon(pm));
-			}
-		} catch (OutOfMemoryError oom) {
-		}
-		return null;
-	}
+        icons = new ArrayList<Drawable>(apps.size());
+        try {
+            for (int i = 0; i < apps.size(); i++) {
+                icons.add(apps.get(i).loadIcon(pm));
+            }
+        } catch (OutOfMemoryError oom) {
+        }
+        return null;
+    }
 
 }
